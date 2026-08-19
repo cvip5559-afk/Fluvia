672c59f096c807c1ac03f2b90742387e640159f4
znndev <ntsara05@gmail.com>
2026-08-19 13:08:33 +0300

    Add files via upload

---
+++ FluviaFinal1App.swift
@@ -0,0 +1,17 @@
+//
+//  FluviaFinal1App.swift
+//  FluviaFinal1
+//
+//  Created by Aleen Aldosari on 28/02/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct fluviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchView()
+        }
+    }
+}
